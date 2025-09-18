f6f6ca8de0deb5aea7cb88f83404e199a7366325
mehedi hasan <mhshohan04@gmail.com>
2025-09-18 11:31:38 +0600

    render foodcard by category

--- script.js
+++ script.js
@@ -6,6 +6,56 @@ const loadCategory = () => {
   .catch(err => console.error('Error fetching categories:', err));
 }
 
+const loadFoods = (id) => {
+  // console.log(id);
+  const url = ` https://taxi-kitchen-api.vercel.app/api/v1/categories/${id}`;
+  fetch(url)
+  .then(res => res.json())
+  .then(data => displayFoods(data.foods))
+}
+
+const displayFoods = (foods) => {
+  // console.log(foods);
+  const foodContainer = document.getElementById('food-container');
+  foodContainer.innerHTML = '';
+
+  foods.map(food => {
+    const foodCard = document.createElement('div');
+    foodCard.innerHTML = `
+    <div class="p-5 bg-white flex gap-3 shadow rounded-xl">
+            <div class="img flex-1">
+              <img
+                src=${food.foodImg}
+                alt=""
+                class="w-[160px] rounded-xl h-[160px] object-cover"
+              />
+            </div>
+            <div class="flex-2">
+              <h1 class="text-xl font-bold">
+                ${food.title}
+              </h1>
+
+              <div class="badge badge-warning">${food.category}</div>
+
+              <div class="divider divider-end">
+                <h2 class="text-yellow-600 font-semibold">
+                  $ <span class="price">${food.price}</span> BDT
+                </h2>
+              </div>
+
+              <button class="btn btn-warning">
+                <i class="fa-solid fa-square-plus"></i>
+                Add This Item
+              </button>
+            </div>
+          </div>`;
+
+    foodContainer.appendChild(foodCard);
+  })
+
+
+}
+
 const displayCategory = (categories) => {
   // console.log(categories);
 
@@ -15,7 +65,7 @@ const displayCategory = (categories) => {
   categories.map(category => {
     const categoryCard = document.createElement('div');
     categoryCard.innerHTML = `
-      <button class="btn btn-block shadow btn-category">
+      <button onClick="loadFoods(${category.id})" class="btn btn-block shadow btn-category">
         <img
           src=${category.categoryImg}
           alt=""
